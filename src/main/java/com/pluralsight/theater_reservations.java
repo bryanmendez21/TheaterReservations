7659b25208a0ccb4e79ae7877b9363fbabf8a85a
bryan <bryanmendez21@icloud.com>
2026-04-14 10:44:27 -0400

    Made Scanners to get user input

--- src/main/java/com/pluralsight/theater_reservations.java
+++ src/main/java/com/pluralsight/theater_reservations.java
@@ -1,4 +1,27 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Formatter;
+import java.util.Scanner;
+
 public class theater_reservations {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("What is your full name: ");
+        String fullName = userInput.nextLine();
+
+        System.out.print("Date of show: ");
+        String date = userInput.nextLine();
+        LocalDate dateReserved = LocalDate.parse(date);
+        System.out.println(date);
+
+        System.out.print("Number of tickets: ");
+        int ticketQuantity = userInput.nextInt();
+
+      //  System.out.printf("%f tickets Reserved for",ticketQuantity);
+
+
+    }
 }
